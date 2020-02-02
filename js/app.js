e3f3764f505595c6d9d32e8e759d22b18fe2ced6
Louis Fernando Medina <louisfm16@gmail.com>
2020-02-01 23:58:47 -0800

    Carousel features like, index tracking, small edits to navigation, menus, and links

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
-var flkty;
+let flkty;
+let galleryIndex = '00';
+let menuOpen = false;
 
-document.addEventListener("DOMContentLoaded", function(){
+// Elements
+let menu;
+let menuToggle;
+
+document.addEventListener('DOMContentLoaded', function(){
+    Init();
+
+    menuToggle.addEventListener('click', function() {
+        if(!menuOpen) { // Menu is off so lets turn it on
+            menu.classList.remove('menu--hidden');
+        } 
+        else if(menuOpen) { // Menu is on so lets turn it off
+            menu.classList.add('menu--hidden');
+        }
+
+        menuOpen = !menuOpen;
+    });
+
+    flkty.on( 'change', function() {
+        SetIndex();
+    });
+});
+
+function Init() {
     flkty = new Flickity( '.carousel', {
+        cellSelector: '.carousel__card',
         freeScroll: true,
         prevNextButtons: false,
         pageDots: false,
         // autoPlay: 3000
     });
-});
+
+    menu = document.getElementById('menu');
+    menuToggle = document.getElementById('menu-toggle');
+    galleryIndex = document.getElementById('info__index');
+
+    SetIndex();
+}
+
+function SetIndex() {
+    let index = `0${(flkty.selectedIndex + 1)}`;
+    galleryIndex.innerHTML = index;
+}
